9b4051e72c0bd56b427407b3dc13b169ba0b9468
Pinapelz <yukais@pinapelz.com>
2024-12-05 13:04:47 -0800

    migrate print to python3

--- server/server-mac-osculator.py
+++ server/server-mac-osculator.py
@@ -16,7 +16,7 @@ class I2DXWebSocketOSC(libi2dx.I2DXWebSocket):
 		try:
 			client.send(msg)
 		except OSC.OSCClientError:
-			print "cannot send OSC message"
+			print("cannot send OSC message")
 
 if __name__ == "__main__":
 	libi2dx.serve(I2DXWebSocketOSC)
